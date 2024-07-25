ac04bd1dd9a2467aad47886dcdac11f013c47d34
newdynamicAshish <ashish.m@newdynamicstech.com>
2024-07-25 17:24:31 +0530

    Initial Commit

---
+++ WeatherAppDemo/WeatherAppDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppDemoApp.swift
+//  WeatherAppDemo
+//
+//  Created by apple on 25/07/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
